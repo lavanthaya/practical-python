ba91e4b87d8526e07b1b219abf008e2b4bf44e71
Lavan Thaya <lavan012@gmail.com>
2021-05-17 20:36:01 -0230

    Exercise 1.9 done

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,6 +2,7 @@
 #
 # Exercise 1.7
 # Exercise 1.8
+# Exercise 1.9
 
 principal = 500000.0
 rate = 0.05
@@ -9,9 +10,13 @@ payment = 2684.11
 total_paid = 0.0
 month = 0
 
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
 while principal > 0:
     month +=1
-    if month <=12:
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
        payment = 2684.11 + 1000.00
     else:
        payment = 2684.11
