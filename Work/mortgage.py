c66a13188327bd552e8b1edcb6fa86da3c1b04a8
Lavan Thaya <lavan012@gmail.com>
2021-05-17 20:39:37 -0230

    Exercise 1.10 done

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -24,6 +24,7 @@ while principal > 0:
     #print(month,payment)
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
+    print (month, round(total_paid,2), round(principal,2))
 
 
 print('Total paid', round(total_paid,2))
