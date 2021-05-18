2415229ba0873d18d70cc8fbdf2cf355a1fb3d5a
Lavan Thaya <lavan012@gmail.com>
2021-05-17 21:34:51 -0230

    Exercise 1.27 done

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+tot_cost = 0
+
+f = open('Data/portfolio.csv', 'rt')
+next(f).split(',')
+
+for line in f:
+    row = line.split(',')
+    #print(row[2])
+    tot_cost += (float(row[2])*float(row[1])) 
+
+f.close()
+
+print(f'Total cost {tot_cost:0.2f}')
