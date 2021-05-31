8f91d28182bfbd56d55f67a2a69d534fa97f9d01
Lavan Thaya <lavan012@gmail.com>
2021-05-31 00:18:14 -0230

    Exercise 3.6

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,35 +4,41 @@
 
 import csv
 
-def parse_csv(filename, select=None, types=[str, int, float]):
+def parse_csv(filename, select=None, types=[str, int, float], has_headers=False):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
-        headers = next(rows)
-        
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
-
-        
         records = []
-        for row in rows:
-            if not row:    # Skip rows with no data
-                continue
-                
-            if indices:
-                row = [ row[index] for index in indices ]
-            
-            if types:
-                row = [func(val) for func, val in zip(types, row) ]
+        # Read the file headers
+        if not has_headers:
+            headers = next(rows)
+        
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
 
-            record = dict(zip(headers, row))
-            records.append(record)
+        
+            
+            for row in rows:
+                if not row:    # Skip rows with no data
+                    continue
+
+                if indices:
+                    row = [ row[index] for index in indices ]
+
+                if types:
+                    row = [func(val) for func, val in zip(types, row) ]
+
+
+                record = dict(zip(headers, row))
+                records.append(record)
+                
+        else:  
+            records = list(map(tuple, rows))
 
     return records
